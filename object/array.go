57c4b65caea841ebb821e3b86186b662077d3bff
MichaelHu <michael.hu@withinspace.com>
2021-03-31 10:32:20 +0800

    add array

--- object/array.go
+++ object/array.go
@@ -1,4 +1,30 @@
 package object
 
-
 type Array []interface{}
+
+
+func InArray(needle interface{}, hystack interface{}) bool {
+	switch key := needle.(type) {
+	case string:
+		for _, item := range hystack.([]string) {
+			if key == item {
+				return true
+			}
+		}
+	case int:
+		for _, item := range hystack.([]int) {
+			if key == item {
+				return true
+			}
+		}
+	case int64:
+		for _, item := range hystack.([]int64) {
+			if key == item {
+				return true
+			}
+		}
+	default:
+		return false
+	}
+	return false
+}
