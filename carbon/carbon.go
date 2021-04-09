0733baeb5344e59d462e656077ca6d7eca6e4d4b
MichaelHu <michael.hu@withinspace.com>
2021-04-09 10:09:58 +0800

    add carbon now

--- carbon/carbon.go
+++
@@ -1,6 +0,0 @@
-package carbon
-
-
-
-const DATETIME_FORMAT = "Y-m-d H:i:s"
-const TIME_FORMAT = "H:i:s"
--- fmt/format.go
+++ fmt/format.go
@@ -32,23 +32,25 @@ func Dump(datas ...interface{}) {
 
 func dump(data interface{}) {
 	var (
+		prettyJson interface{}
 		strData string
 		err     error
 	)
+	if data==nil{
+		fmt.Print("[nil]\r\n")
+	}
 	if reflect.TypeOf(data).Kind() != reflect.String {
-		////fmt.Printf("dump data: %+v\r\n", data)
-		strData = fmt.Sprintf("%+v", data)
-		//fmt.Printf("dump convert string: %+v\r\n", strData)
+		prettyJson,err = PrettyJson(data)
 
 	} else {
 		strData = data.(string)
+		prettyJson,err = PrettyJson(strData)
 	}
 
-	prettyJson, err := PrettyJson(strData)
 	if err != nil {
-		fmt.Printf("convert pretty fmt error:%v", err)
+		fmt.Printf("convert pretty fmt error:%v \r\n", err)
 	}
-	fmt.Printf("%+v", prettyJson)
+	fmt.Printf("%+v \r\n", prettyJson)
 }
 
 
