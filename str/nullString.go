83ade914528a22c0cfe61eddff454533417b1370
Matrix-X <matrix-x@artisancloud.com>
2021-08-04 14:40:27 +0800

    update

---
+++ str/nullString.go
@@ -0,0 +1,41 @@
+package str
+
+import (
+	"database/sql"
+	"encoding/json"
+)
+
+type NullString struct {
+	sql.NullString
+}
+
+func NewNullString(String string, Valid bool) NullString {
+	return NullString{
+		sql.NullString{
+			String: String,
+			Valid:  Valid,
+		},
+	}
+}
+
+func (v *NullString) MarshalJSON() ([]byte, error) {
+	if v.Valid {
+		return json.Marshal(v.String)
+	} else {
+		return json.Marshal(nil)
+	}
+}
+
+func (v NullString) UnmarshalJSON(data []byte) error {
+	var s *string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	if s != nil {
+		v.Valid = true
+		v.String = *s
+	} else {
+		v.Valid = false
+	}
+	return nil
+}
