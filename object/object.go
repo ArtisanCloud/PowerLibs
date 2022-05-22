2c9b3d702cf00309ed8b0906c6269c66f3cb2f52
Matrix-X <matrix-x@artisancloud.com>
2022-05-22 15:20:40 +0800

    feature(object): encode to byte

---
+++ object/object.go
@@ -0,0 +1,19 @@
+package object
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"log"
+)
+
+func EncodeToBytes(p interface{}) []byte {
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("uncompressed size (bytes): ", len(buf.Bytes()))
+	return buf.Bytes()
+}
