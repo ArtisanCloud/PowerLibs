42d4e6b053ccfcaf7c3f44895e5adc8023753abf
Matrix-X <matrix-x@artisancloud.com>
2022-05-28 17:11:07 +0800

    feature(carbonPeriod): add diffInDays

---
+++ data/csv.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"bytes"
+	"encoding/csv"
+	"os"
+)
+
+func CSVEncode(arrayCSV [][]string) ([]byte, error) {
+
+	buffer := new(bytes.Buffer)
+	writer := csv.NewWriter(buffer)
+
+	err := writer.WriteAll(arrayCSV)
+
+	return buffer.Bytes(), err
+
+}
+
+func CSVEncodeToFile(arrayCSV [][]string, file *os.File) ( error) {
+
+	writer := csv.NewWriter(file)
+
+	err := writer.WriteAll(arrayCSV)
+
+	return err
+
+}
