00b0d554b2dbe165b3f393fbd27c04c54b8ec704
Matrix-X <matrix-x@artisancloud.com>
2023-01-08 00:10:14 +0800

    feat(file): add ConvertFileHandleToReader

--- os/file.go
+++ os/file.go
@@ -1,8 +1,11 @@
 package os
 
 import (
+	"bytes"
+	"errors"
 	"io"
 	"os"
+	"strings"
 )
 
 // https://www.golangprograms.com/files-directories-examples.html
@@ -33,4 +36,21 @@ func CopyFile(src string, dst string) (err error) {
 func MoveFile(src string, dst string) (err error) {
 	err = os.Rename(src, dst)
 	return err
+}
+
+func ConvertFileHandleToReader(obj interface{}) (data io.Reader, err error) {
+	switch obj.(type) {
+	case string:
+		return strings.NewReader(obj.(string)), nil
+
+	case []byte:
+		return bytes.NewReader(obj.([]byte)), nil
+
+	case io.Reader:
+		return obj.(io.Reader), nil
+
+	default:
+		return nil, errors.New("not support file data")
+	}
+
 }
